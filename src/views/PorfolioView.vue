5274ea36d4b2161b854ac12cbe22ef7a68512205
Jd2698 <jamer.delgado@dreamcodesoft.com>
2025-03-23 11:12:29 -0500

    update image format from png to webp

--- src/views/PorfolioView.vue
+++ src/views/PorfolioView.vue
@@ -2,47 +2,47 @@
 	import Carousel from "@/components/Carousel.vue";
 
 	const appointmentsPictures = [
-		{ src: "images/appointments/1.welcome.png", alt: "Image 1" },
-		{ src: "images/appointments/2.login.png", alt: "Image 2" },
-		{ src: "images/appointments/3.dashboard.png", alt: "Image 3" },
-		{ src: "images/appointments/4.appointments.png", alt: "Image 4" },
+		{ src: "images/appointments/1.welcome.webp", alt: "Image 1" },
+		{ src: "images/appointments/2.login.webp", alt: "Image 2" },
+		{ src: "images/appointments/3.dashboard.webp", alt: "Image 3" },
+		{ src: "images/appointments/4.appointments.webp", alt: "Image 4" },
 		{
-			src: "images/appointments/5.appointments-info.png",
+			src: "images/appointments/5.appointments-info.webp",
 			alt: "Image 5",
 		},
 		{
-			src: "images/appointments/6.appointment-form.png",
+			src: "images/appointments/6.appointment-form.webp",
 			alt: "Image 6",
 		},
-		{ src: "images/appointments/8.user-form.png", alt: "Image 7" },
+		{ src: "images/appointments/8.user-form.webp", alt: "Image 7" },
 		{
-			src: "images/appointments/12.specialty-form.png",
+			src: "images/appointments/12.specialty-form.webp",
 			alt: "Image 8",
 		},
-		{ src: "images/appointments/13.client.png", alt: "Image 9" },
+		{ src: "images/appointments/13.client.webp", alt: "Image 9" },
 	];
 
 	const ecommercePictures = [
-		{ src: "images/ecommerce/home.png", alt: "Image 1" },
-		{ src: "images/ecommerce/cart.png", alt: "Image 2" },
-		{ src: "images/ecommerce/product.png", alt: "Image 3" },
-		{ src: "images/ecommerce/user-modal.png", alt: "Image 4" },
+		{ src: "images/ecommerce/home.webp", alt: "Image 1" },
+		{ src: "images/ecommerce/cart.webp", alt: "Image 2" },
+		{ src: "images/ecommerce/product.webp", alt: "Image 3" },
+		{ src: "images/ecommerce/user-modal.webp", alt: "Image 4" },
 	];
 
 	const javaEcommercePictures = [
-		{ src: "images/ecommerce-java/1.login.png", alt: "Image 1" },
-		{ src: "images/ecommerce-java/2.general.png", alt: "Image 2" },
-		{ src: "images/ecommerce-java/3.profile.png", alt: "Image 3" },
-		{ src: "images/ecommerce-java/4.users.png", alt: "Image 4" },
+		{ src: "images/ecommerce-java/1.login.webp", alt: "Image 1" },
+		{ src: "images/ecommerce-java/2.general.webp", alt: "Image 2" },
+		{ src: "images/ecommerce-java/3.profile.webp", alt: "Image 3" },
+		{ src: "images/ecommerce-java/4.users.webp", alt: "Image 4" },
 		{
-			src: "images/ecommerce-java/6.users_form2.png",
+			src: "images/ecommerce-java/6.users_form2.webp",
 			alt: "Image 6",
 		},
-		{ src: "images/ecommerce-java/7.products.png", alt: "Image 7" },
-		{ src: "images/ecommerce-java/8.tables.png", alt: "Image 8" },
-		{ src: "images/ecommerce-java/9.orders.png", alt: "Image 9" },
+		{ src: "images/ecommerce-java/7.products.webp", alt: "Image 7" },
+		{ src: "images/ecommerce-java/8.tables.webp", alt: "Image 8" },
+		{ src: "images/ecommerce-java/9.orders.webp", alt: "Image 9" },
 		{
-			src: "images/ecommerce-java/10.orders_history.png",
+			src: "images/ecommerce-java/10.orders_history.webp",
 			alt: "Image 10",
 		},
 	];
