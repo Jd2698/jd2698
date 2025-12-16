0f566f7180ef33823b28e33b9add6ef6809805ad
Jamer Delgado <133704939+Jd2698@users.noreply.github.com>
2025-12-16 16:26:19 -0500

    update image size

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -24,7 +24,7 @@ const titles = [
   <section id="aboutMe" class="text-black dark:text-white">
     <img
       src="../../public/images/me.webp"
-      class="size-44 border-2 object-contain rounded-full transition-[transform,box-shadow] duration-300 hover:-translate-y-1 hover:shadow-[0_25px_50px_rgba(0,0,0,.2)]"
+      class="size-32 border-2 object-contain rounded-full transition-[transform,box-shadow] duration-300 hover:-translate-y-1 hover:shadow-[0_25px_50px_rgba(0,0,0,.2)]"
       alt="me"
     />
 
